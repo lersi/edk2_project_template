60fa5638093efc25526f0b3a1be0997be36b2962
Lior Shalmay <lior.shalmay@intel.com>
2022-02-06 14:56:52 +0200

    fixed build issues

--- _build.bat
+++ _build.bat
@@ -1,22 +1,22 @@
 @echo off
 @REM arguments:  
-@REM 1) <path to edk2>
-@REM 2) <visual studio version>
-@REM 3) <path to build dir>
+@REM 1) <path to build dir>
+@REM 2) <path to edk2>
+@REM 3) <visual studio version>
 @REM 4) <path to packages dir> 
 @REM ) 
 @REM rest)  <parms to build>
 
 @REM fix wierd issue with path
-set PYTHON_COMMAND="%PYTHON_COMMAND%"
+if not ^" EQU !PYTHON_COMMAND:~1! set PYTHON_COMMAND="%PYTHON_COMMAND%"
 
-set WORKSPACE=%3
-set PACKAGES_PATH=%1;%4
-call %1\edksetup.bat %2
+set WORKSPACE=%1
+set PACKAGES_PATH=%2;%4
+call %2\edksetup.bat %3
 
 if not exist %EDK_TOOLS_BIN%\ (
     echo "build_tools does not exists rebuilding them"
-    call %1\edksetup.bat Rebuild %2
+    call %2\edksetup.bat Rebuild %3
 )
 set RESTVAR=
 shift
@@ -33,4 +33,4 @@ echo packages %PACKAGES_PATH%
 echo edk_tools %EDK_TOOLS_BIN%
 echo python command: %PYTHON_COMMAND% 
 
-call build.bat %RESTVAR%
+call %BASE_TOOLS_PATH%\BinWrappers\WindowsLike\build.bat %RESTVAR%
--- cmake_files/include_edk2.cmake
+++ cmake_files/include_edk2.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.11)
+cmake_minimum_required(VERSION 3.17)
 include(FetchContent)
 if(NOT DEFINED LOCAL_EDK2)
     set(EDK_REPO https://github.com/tianocore/edk2.git)
@@ -13,6 +13,8 @@ FetchContent_Declare(
     edk2
     GIT_REPOSITORY ${EDK_REPO}
     GIT_TAG        ${EDK2_TAG}
+    GIT_SUBMODULES_RECURSE  FALSE
+    GIT_PROGRESS            TRUE
 )
 
 FetchContent_Populate(edk2)
