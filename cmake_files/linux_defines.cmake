c9eada85140764c57056ea65e9a47894fcb2db8a
lersi <liorshalmay1@gmail.com>
2022-08-31 12:50:42 +0700

    added documentation

--- cmake_files/linux_defines.cmake
+++ cmake_files/linux_defines.cmake
@@ -18,13 +18,18 @@ if(NOT DEFINED TOOL_CHAIN)
         OUTPUT_VARIABLE GCC_VERSION
     )
 
+    
     if(${GCC_VERSION} VERSION_LESS 4.8)
+    # edk2 requires gcc 4.8 and later
         message(FATAL_ERROR "EDK2 required gcc version of 4.8 or later")
     elseif(${GCC_VERSION} VERSION_LESS 4.9)
+    # gcc is version 4.8.x
         set(TOOL_CHAIN "GCC48" CACHE INTERNAL "")
     elseif(${GCC_VERSION} VERSION_LESS 5.0)
+    # gcc is version 4.9.x
         set(TOOL_CHAIN "GCC49" CACHE INTERNAL "")
     else()
+    # gcc is version 5.0 or later
         set(TOOL_CHAIN "GCC5" CACHE INTERNAL "")
     endif()
     
