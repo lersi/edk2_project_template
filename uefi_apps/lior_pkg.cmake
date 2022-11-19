37cfd0504d3eb15c1265df9ac11446a8ad57f6ca
lior <lior.serverces@gmail.com>
2022-11-19 23:42:37 +0200

    added support for multiple targets

--- uefi_apps/lior_pkg.cmake
+++ uefi_apps/lior_pkg.cmake
@@ -1,15 +1,15 @@
 cmake_minimum_required(VERSION 3.12)
 # the name of the package, this must be idendical to the name of the package dir
 set(PACKAGE_NAME LiorPkg)
+set(ARCH X64)
 
 # build arguments for edk2 build system
 set(BUILD_ARGS 
-    --arch=X64 # architecture to compile to
     --platform=LiorPkg/lior.dsc # the platform to compile
     # --module=LiorPkg/Lior/lior.inf # the specific module to compile
     -n ${CORE_COUNT} # amount of threads
     --tagname=${TOOL_CHAIN} # tool chain to use
 )
 # this would create a target for the package
-add_package(${PACKAGE_NAME} ${BUILD_ARGS})
+add_package(${PACKAGE_NAME} ${ARCH} ${BUILD_ARGS})
 
